437d94e9d5d245cbd21b6085a38c4d50137ee522
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-22 15:05:06 -0700

    functionality works, only half of tests pass

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,29 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import Card from "../components/ReusableCard";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/directors")
+      .then(r => r.json())
+      .then(data => setDirectors(data))
+      .catch(error => console.error(error));
+  }, [])
+
+  const directorList = directors.map(director =>{
+    return <Card key={director.id} director={director.name} movies={director.movies}/>
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directorList}
       </main>
     </>
   );
